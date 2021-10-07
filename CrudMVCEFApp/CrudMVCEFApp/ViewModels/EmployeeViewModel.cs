4503e62d9dbcc43716300c4f22be25f10729e268
nthgiang181 <Giang.NguyenThiHuong@nttdata.com>
2021-10-08 03:03:35 +0700

    crud using entity framework

---
+++ CrudMVCEFApp/CrudMVCEFApp/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrudMVCEFApp.Models;
+
+namespace CrudMVCEFApp.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public IEnumerable<Department> Department { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
